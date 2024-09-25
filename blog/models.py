9e46288a5e5f37c35e8e3ff1cc85bab8d425596c
DORSCORD <dorscord356@gmail.com>
2024-09-25 18:59:08 +0200

    Add admin and view

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -10,6 +15,11 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -19,6 +29,9 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ["-publish"]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        "blog/post/list.html",
+        {"posts": posts}
+    )
+
+def post_detali(request, id):
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status = Post.Status.PUBLISHED
+    )
+    
+    return render(
+        request,
+        "blog/post/detali.html",
+        {"post": post}
+    )
