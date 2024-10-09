f98227d87758f8237c42b59db5edc693fee6c296
DORSCORD <dorscord356@gmail.com>
2024-10-09 19:05:52 +0200

    Add Anco

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 
+class PostListView(ListView):
+    """Алтернвтивне видання списку постів"""
+    queryset = Post.published.all()
+
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Посторінкове розбиття з 3 постами на сторінках
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(
         request,
         "blog/post/list.html",
         {"posts": posts}
     )
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status = Post.Status.PUBLISHED
+        status = Post.Status.PUBLISHED,
+        slug=post,
+        publish__year = year,
+        publish__month = month,
+        publish__day = day,
     )
     
     return render(
@@ -24,3 +48,14 @@ def post_detail(request, id):
         "blog/post/detail.html",
         {"post": post}
     )
+def post_share(request, post_id):
+    # Отримати пост за id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == "POST":
+        # Форма передана на обробку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, "blog/post/share.html", {"post": post, "form": form})
